c439fcb22e1bff0bbce84fc7c0419455cf137571
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2025-01-25 19:07:25 +0530

    tinyMCE

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name , control , label , defaultValue=""}) {
            control={control}
            render={({field:{onChange}})=>(
             <Editor
-             apiKey='6h3e9phwual7zpbskkjlkv928s595jlw9ae24ma3ahv3syee'
+            apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
